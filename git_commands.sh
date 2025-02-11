756c0ce922fed21d9c25626c847ea142fd435be2
Atul Karn <atulkarn@Atuls-Mac-mini.local>
2025-02-11 07:27:18 +0530

    Modified files: git_commands.sh

--- git_commands.sh
+++ git_commands.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Get the name of the most recent modified file
-FILE_NAME=$(ls -t | head -n 1)
+# Get the list of modified files that are staged or unstaged for commit
+MODIFIED_FILES=$(git status -s | grep -E '^\s*M' | awk '{print $2}' | tr '\n' ' ')
 
-# If there are files to commit
-if [ -n "$FILE_NAME" ]; then
-  # Add changes to git
+# If there are modified files
+if [ -n "$MODIFIED_FILES" ]; then
+  # Add all modified files to the staging area
   git add .
 
-  # Commit with the file name as the commit message
-  git commit -m "$FILE_NAME"
+  # Commit with the modified files as the commit message
+  git commit -m "Modified files: $MODIFIED_FILES"
 
   # Push changes to the remote repository
   git push
 else
-  echo "No files found to commit."
+  echo "No modified files found to commit."
 fi
