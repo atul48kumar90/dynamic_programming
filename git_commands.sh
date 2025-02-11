7051f0f4869ed929e550fd98bb4454cc4a64f546
Atul Karn <atulkarn@Atuls-Mac-mini.local>
2025-02-11 08:41:16 +0530

    Files changed: git_commands.sh

--- git_commands.sh
+++ git_commands.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-# Get the list of modified files that are staged or unstaged for commit
-MODIFIED_FILES=$(git status -s | grep -E '^\s*M' | awk '{print $2}' | tr '\n' ' ')
+# Get the list of modified (staged and unstaged) files
+MODIFIED_FILES=$(git status -s | grep -E '^\s*[AM]' | awk '{print $2}' | tr '\n' ' ')
 
-# If there are modified files
+# If there are modified or untracked files
 if [ -n "$MODIFIED_FILES" ]; then
-  # Add all modified files to the staging area
+  # Add all modified and untracked files to the staging area
   git add .
 
-  # Commit with the modified files as the commit message
-  git commit -m "Modified files: $MODIFIED_FILES"
+  # Commit with the modified/untracked files as the commit message
+  git commit -m "Files changed: $MODIFIED_FILES"
 
   # Push changes to the remote repository
   git push
 else
-  echo "No modified files found to commit."
+  echo "No files found to commit."
 fi
---
+++ striver/FrogJumpI.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class FrogJumpI {
+    public static void main(String[] args) {
+        int n = 7;
+        int[] arr = {7, 4, 4, 2, 6, 6, 3, 4 };
+        System.out.println("array: " + arr);
+
+        int mem[] = new int[8];
+        Arrays.fill(mem, -1);
+        int res = f(arr, mem, n);
+
+        System.out.println("min energy taken by from is: " + res);
+    }
+
+    public static int f(int[] arr, int[] mem, int n)
+    {
+        int right = Integer.MAX_VALUE;
+
+        if(n==0) return 0;
+
+        if(mem[n] != -1) return mem[n];
+        
+        int left = f(arr, mem, n-1) + Math.abs(arr[n] - arr[n-1]);
+        if(n>1)
+        {
+            right = f(arr, mem, n-2) + Math.abs(arr[n] - arr[n-2]);
+        }
+        mem[n] = Math.min(left, right);
+        return mem[n];
+
+    }
+}
