0ed6bddfb18c27cf227a309b737113a5e5fbe49c
Atul Karn <atulkarn@Atuls-Mac-mini.local>
2025-02-16 11:25:25 +0530

    partition with given diff

---
+++ striver/PartitionWithGivenDifference.java
@@ -0,0 +1,78 @@
+/*
+ * Problem statement
+Given an array ‘ARR’, partition it into two subsets (possibly empty) such that their union is the original array. Let the sum of the elements of these two subsets be ‘S1’ and ‘S2’.
+
+Given a difference ‘D’, count the number of partitions in which ‘S1’ is greater than or equal to ‘S2’ and the difference between ‘S1’ and ‘S2’ is equal to ‘D’. Since the answer may be too large, return it modulo ‘10^9 + 7’.
+
+If ‘Pi_Sj’ denotes the Subset ‘j’ for Partition ‘i’. Then, two partitions P1 and P2 are considered different if:
+
+1) P1_S1 != P2_S1 i.e, at least one of the elements of P1_S1 is different from P2_S2.
+2) P1_S1 == P2_S2, but the indices set represented by P1_S1 is not equal to the indices set of P2_S2. Here, the indices set of P1_S1 is formed by taking the indices of the elements from which the subset is formed.
+Refer to the example below for clarification.
+Note that the sum of the elements of an empty subset is 0.
+
+For example :
+If N = 4, D = 3, ARR = {5, 2, 5, 1}
+There are only two possible partitions of this array.
+Partition 1: {5, 2, 1}, {5}. The subset difference between subset sum is: (5 + 2 + 1) - (5) = 3
+Partition 2: {5, 2, 1}, {5}. The subset difference between subset sum is: (5 + 2 + 1) - (5) = 3
+These two partitions are different because, in the 1st partition, S1 contains 5 from index 0, and in the 2nd partition, S1 contains 5 from index 2.
+ */
+
+import java.util.*;
+
+
+class PartitionWithGivenDifference{
+    
+static int mod =(int)(Math.pow(10,9)+7);
+static int countPartitionsUtil(int ind, int target,int[] arr, int[][] dp){
+
+     if(ind == 0){
+        if(target==0 && arr[0]==0)
+            return 2;
+        if(target==0 || target == arr[0])
+            return 1;
+        return 0;
+    }
+    
+    if(dp[ind][target]!=-1)
+        return dp[ind][target];
+        
+    int notTaken = countPartitionsUtil(ind-1,target,arr,dp);
+    
+    int taken = 0;
+    if(arr[ind]<=target)
+        taken = countPartitionsUtil(ind-1,target-arr[ind],arr,dp);
+        
+    return dp[ind][target]= (notTaken + taken)%mod;
+}
+
+static int countPartitions(int d,int[] arr){
+    int n = arr.length;
+    int totSum = 0;
+    for(int i=0; i<arr.length;i++){
+        totSum += arr[i];
+    }
+    
+    //Checking for edge cases
+    if(totSum-d<0) return 0;
+    if((totSum-d)%2==1) return 0;
+    
+    int s2 = (totSum-d)/2;
+    
+    int dp[][] = new int[n][s2+1];
+    
+    for(int row[]: dp)
+    Arrays.fill(row,-1);
+    
+    return countPartitionsUtil(n-1,s2,arr,dp);
+}
+
+public static void main(String args[]) {
+
+  int arr[] = {5,2,6,4};
+  int d=3;
+                                 
+  System.out.println("The number of subsets found are "+countPartitions(d,arr));
+}
+}
