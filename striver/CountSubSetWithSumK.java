8f8233d229cebb8bf918171c34203335d096a4d0
Atul Karn <atulkarn@Atuls-Mac-mini.local>
2025-02-16 11:10:27 +0530

    fix with 0 array input

--- striver/CountSubSetWithSumK.java
+++ striver/CountSubSetWithSumK.java
@@ -13,34 +13,40 @@ Since the number of ways can be very large, print it modulo 10 ^ 9 + 7.
 
  */
 
-import java.util.*;
+ import java.util.*;
 
-public class CountSubSetWithSumK {
-    public static int findWays(int num[], int target) {
-        int n = num.length;
-        int[][] dp = new int[n][target + 1];
-
-        for (int[] row : dp) {
-            Arrays.fill(row, -1);
-        }
-
-        return countSubsets(n - 1, target, num, dp);
-    }
-
-    public static int countSubsets(int ind, int target, int[] num, int[][] dp) {
-        if (target == 0) return 1; // Found a valid subset
-        if (ind < 0) return 0; // No elements left
-        if (dp[ind][target] != -1) return dp[ind][target];
-
-        // Not picking the current element
-        int not_pick = countSubsets(ind - 1, target, num, dp);
-
-        // Picking the current element (allow zeros)
-        int pick = 0;
-        if (num[ind] <= target) { // Fix: Ensure we don't go negative
-            pick = countSubsets(ind - 1, target - num[ind], num, dp);
-        }
-
-        return dp[ind][target] = pick + not_pick;
-    }
-}
+ public class CountSubSetWithSumK {
+     public static int findWays(int num[], int target) {
+         int n = num.length;
+         int[][] dp = new int[n][target + 1];
+ 
+         for (int[] row : dp) {
+             Arrays.fill(row, -1);
+         }
+ 
+         return countSubsets(n - 1, target, num, dp);
+     }
+ 
+     public static int countSubsets(int ind, int target, int[] num, int[][] dp) {
+         //if(ind<0) return 0;
+         if(ind == 0)
+         {
+             if(target == 0 && num[0] == 0) return 2;
+             if(target == 0 || target == num[0]) return 1;
+             return 0;
+         }
+         if (dp[ind][target] != -1) return dp[ind][target];
+ 
+         // Not picking the current element
+         int not_pick = countSubsets(ind - 1, target, num, dp);
+ 
+         // Picking the current element (allow zeros)
+         int pick = 0;
+         if (num[ind] <= target) { // Fix: Ensure we don't go negative
+             pick = countSubsets(ind - 1, target - num[ind], num, dp);
+         }
+ 
+         return dp[ind][target] = pick + not_pick;
+     }
+ }
+ 
