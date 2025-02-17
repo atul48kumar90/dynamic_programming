93cae3f669b9d5c55a2c0e975edcfca474924c4b
Atul Karn <atulkarn@Atuls-Mac-mini.local>
2025-02-17 10:00:34 +0530

    minimum insertion to make string palindromic

---
+++ striver/MinimumInsertionToMakeStringPalindromic.java
@@ -0,0 +1,61 @@
+/*
+ * Given a string s. In one step you can insert any character at any index of the string.
+
+Return the minimum number of steps to make s palindrome.
+
+A Palindrome String is one that reads the same backward as well as forward.
+
+ 
+
+Example 1:
+
+Input: s = "zzazz"
+Output: 0
+Explanation: The string "zzazz" is already palindrome we do not need any insertions.
+Example 2:
+
+Input: s = "mbadm"
+Output: 2
+Explanation: String can be "mbdadbm" or "mdbabdm".
+Example 3:
+
+Input: s = "leetcode"
+Output: 5
+Explanation: Inserting 5 characters the string becomes "leetcodocteel".
+ */
+
+
+class MinimumInsertionToMakeStringPalindromic {
+    public int minInsertions(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        sb.reverse();
+        String t = sb.toString();
+        int n = f(s, t);
+        return s.length() - n;
+    }
+
+    public int f(String s, String t)
+    {
+        int n = s.length();
+        int m = t.length();
+
+        int[][] dp = new int[n+1][m+1];
+
+        for(int j=0; j<=m; j++) dp[0][j] = 0;
+        for(int i=0; i<=n; i++) dp[i][0] = 0;
+
+        for(int i=1; i<=n; i++)
+        {
+            for(int j=1; j<=m; j++)
+            {
+                if(s.charAt(i-1) == t.charAt(j-1))
+                {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+}
